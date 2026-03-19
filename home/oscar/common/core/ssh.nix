44fd9260d45666cee229098a42e11d52be390da9
David Lim <david@networkshokunin.com>
2026-03-19 18:30:13 +0800

    updated

--- home/oscar/common/core/ssh.nix
+++ home/oscar/common/core/ssh.nix
@@ -7,14 +7,19 @@
    {
       enable = true;
 
-      controlMaster = "auto";
-      controlPath = "${config.home.homeDirectory}/.ssh/sockets/S.%r@%h:%p";
-      controlPersist = "20m";
-      # Avoids infinite hang if control socket connection interrupted. ex: vpn goes down/up
-      serverAliveCountMax = 3;
-      serverAliveInterval = 5; # 3 * 5s
-      hashKnownHosts = true;
-      addKeysToAgent = "yes";
+      matchBlock = {
+         "*" = {
+            controlMaster = "auto";
+            controlPath = "${config.home.homeDirectory}/.ssh/sockets/S.%r@%h:%p";
+            controlPersist = "20m";
+            # Avoids infinite hang if control socket connection interrupted. ex: vpn goes down/up
+            serverAliveCountMax = 3;
+            serverAliveInterval = 5; # 3 * 5s
+            hashKnownHosts = true;
+            addKeysToAgent = "yes";
+         };
+
+
    };
   home.file = {
     ".ssh/config.d/.keep".text = "# Managed by Home Manager";
