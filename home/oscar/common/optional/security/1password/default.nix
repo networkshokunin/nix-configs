fb91608447a95b828286ecffe191a73972a95c62
David Lim <david@networkshokunin.com>
2026-03-19 22:33:19 +0800

    updated

---
+++ home/oscar/common/optional/security/1password/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  ...
+}:
+{
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "${config.hostSpec.username}" ];
+  };
+
+  environment.etc = {
+        "1password/custom_allowed_browsers" = {
+          text = ''
+            vivaldi-bin
+          '';
+          mode = "0755";
+        };
+      };
+}
