130b2c70778f738112b23e06a992ce76d739a929
David Lim <david@networkshokunin.com>
2026-03-19 18:58:18 +0800

    updated

--- home/oscar/common/core/nixos.nix
+++ home/oscar/common/core/nixos.nix
@@ -1,8 +1,8 @@
 # FIXME(starter): the declarations here will ONLY be applied to Linux-based machines.
 # Core home functionality that will only work on Linux
 {
-  ...
+  pkgs,...
 }:
 {
-
+  programs.ghostty.package = pkgs.ghostty;
 }
