fe5f6e3777c7dca2deeae8b846006db8c0d8bc9f
David Lim <david@networkshokunin.com>
2026-03-20 09:27:02 +0800

    updated

--- hosts/common/optional/services/cosmic.nix
+++ hosts/common/optional/services/cosmic.nix
@@ -1,5 +1,5 @@
 {
-  pkgs, ...
+  pkgs, config, ...
 }:
 {
   # Enable the COSMIC login manager
@@ -8,11 +8,11 @@
   # Enable the COSMIC desktop environment
   services.desktopManager.cosmic.enable = true;
 
-  # services.displayManager.autoLogin = {
-  #   enable = true;
-  #   # Replace `yourUserName` with the actual username of user who should be automatically logged in
-  #   user = "yourUserName";
-  # };
+  services.displayManager.autoLogin = {
+    enable = true;
+    # Replace `yourUserName` with the actual username of user who should be automatically logged in
+    user = "${config.hostSpec.username}";
+  };
 
   environment.cosmic.excludePackages = with pkgs; [
     cosmic-edit
