d87a8592ee81361626ca947c6d35315358da73df
David Lim <david@networkshokunin.com>
2026-03-20 09:56:02 +0800

    updated

--- hosts/common/users/primary/nixos.nix
+++ hosts/common/users/primary/nixos.nix
@@ -41,7 +41,7 @@ in
   # root's ssh key are mainly used for remote deployment, borg, and some other specific ops
   users.users.root = {
     shell = pkgs.bash;
-    hashedPasswordFile = config.users.users.${hostSpec.username}.hashedPasswordFile;
+    hashedPasswordFile = config.sops.secrets."passwords/root".path;
     #hashedPassword = config.users.users.${hostSpec.username}.hashedPassword; # This comes from hosts/common/optional/minimal.nix and gets overridden if sops is working
     openssh.authorizedKeys.keys = config.users.users.${hostSpec.username}.openssh.authorizedKeys.keys; # root's ssh keys are mainly used for remote deployment.
   };
