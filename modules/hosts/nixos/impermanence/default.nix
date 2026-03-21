0e2845d2e1d94670378dad48339b387f207455a2
David Lim <david@networkshokunin.com>
2026-03-21 16:55:26 +0800

    updated

--- modules/hosts/nixos/impermanence/default.nix
+++ modules/hosts/nixos/impermanence/default.nix
@@ -128,6 +128,8 @@
           "/etc/machine-id"
           "/etc/ssh/ssh_host_ed25519_key"
           "/etc/ssh/ssh_host_ed25519_key.pub"
+          "/etc/ssh/ssh_host_rsa_key"
+          "/etc/ssh/ssh_host_rsa_key.pub"
 
           # Non-essential
           "/root/.ssh/known_hosts"
