5948af7ec39d7a4c5476c04102cba40d2a0d966f
David Lim <david@networkshokunin.com>
2026-03-22 17:57:48 +0800

    updated

--- modules/hosts/nixos/impermanence/default.nix
+++ modules/hosts/nixos/impermanence/default.nix
@@ -14,6 +14,7 @@
       default = config.hostSpec.isImpermanent;
       description = "Enable impermanence";
     };
+    # FIXME: Actually use this in the script, but need to use the substituteAll approach
     removeTmpFilesOlderThan = lib.mkOption {
       type = lib.types.int;
       default = 14;
@@ -39,6 +40,7 @@
     };
   };
 
+  # FIXME(impermanence): Indicate the subvolume to backup
   config = lib.mkIf config.system.impermanence.enable (
     let
       btrfs-diff = pkgs.writeShellApplication {
@@ -46,40 +48,82 @@
         runtimeInputs = lib.attrValues { inherit (pkgs) eza fd btrfs-progs; };
         text = lib.readFile ./btrfs-diff.sh;
       };
+    in
+    {
+
+  boot.initrd = 
+    let
       rollbackScript = pkgs.replaceVars ./rollback-script.sh {
         "@removeTmpFilesOlderThan@" = toString config.system.impermanence.removeTmpFilesOlderThan;
       };
     in
     {
-      boot.initrd = {
-        supportedFilesystems = [ "btrfs" ];
-
-        systemd.services.rollback = {
-          description = "Rollback BTRFS root subvolume to a pristine state";
-          wantedBy = [ "initrd.target" ];
-
-          requires = [ "dev-disk-by\\x2dlabel-BTRFS.device" ];
-          after = [ "dev-disk-by\\x2dlabel-BTRFS.device" ];
-          before = [ "sysroot.mount" ];
-
-          unitConfig.DefaultDependencies = "no";
-          serviceConfig.Type = "oneshot";
-          script = builtins.readFile rollbackScript;
-        };
+      supportedFilesystems = [ "btrfs" ];
+  
+      systemd.services.rollback = {
+        description = "Rollback BTRFS root subvolume to a pristine state";
+        wantedBy = [ "initrd.target" ];
+        
+        # Dependencies
+        requires = [ "dev-disk-by\\x2dlabel-BTRFS.device" ];
+        after = [ "dev-disk-by\\x2dlabel-BTRFS.device" ];
+        before = [ "sysroot.mount" ];
+  
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = rollbackScript;
       };
+    };
+      # NOTE: With boot.initrd.systemd.enable = true, we can't use boot.initrd.postDeviceCommands as per
+      # https://discourse.nixos.org/t/impermanence-vs-systemd-initrd-w-tpm-unlocking/25167
+      # So we build an early stage systemd service, which is modeled after
+      # https://github.com/kjhoerr/dotfiles/blob/trunk/.config/nixos/os/persist.nix
+      # boot.initrd.postDeviceCommands = lib.mkAfter (lib.readFile ./btrfs_wipe_root.sh);
+      # Also see https://github.com/Misterio77/nix-config/blob/main/hosts/common/optional/ephemeral-btrfs.nix
+      # boot.initrd =
+      #   let
+      #     hostname = config.networking.hostName;
+      #     btrfs-subvolume-wipe-src = lib.readFile ./btrfs-wipe-root.sh;
+      #   in
+      #   {
+      #     supportedFilesystems = [ "btrfs" ];
+      #     # https://github.com/Teqed/nixos-config/blob/b71f45af9108aaceb017d75d3daf7b1d1c85d5fe/modules/nixos/impermanence.nix#L129
+      #     systemd.services.btrfs-rollback = {
+      #       description = "Rollback BTRFS root subvolume to a pristine state";
+      #       wantedBy = ["initrd.target"];
+      #       requires = ["dev-disk-by\\x2dlabel-BTRFS.device"];
+      #       wants = ["dev-disk-by\\x2dlabel-BTRFS.device"];
+      #       after = [
+      #         "dev-disk-by\\x2dlabel-BTRFS.device"
+      #       ];
+      #       before = [
+      #         "sysroot.mount"
+      #       ];
+      #       unitConfig.DefaultDependencies = "no";
+      #       serviceConfig.Type = "oneshot";
+      #       script = btrfs-subvolume-wipe-src;
+      #     };
+      #   };
 
       fileSystems."${config.hostSpec.persistFolder}".neededForBoot = true;
 
+      # NOTE: This is a list of directories and files that we want to persist across reboots for all systems
+      # per-system lists are provided in hosts/<host>/
       environment.persistence."${config.hostSpec.persistFolder}" = {
-        hideMounts = true;
+        hideMounts = true; # Temporary disable for debugging
         directories = (
           lib.flatten (
             [
               "/var/log"
-              "/var/lib/bluetooth"
+              "/var/lib/bluetooth" # move to bluetooth-specific
               "/var/lib/nixos"
               "/var/lib/systemd/coredump"
               "/etc/NetworkManager/system-connections"
+
+              # systemd DynamicUser requires /var/{lib,cache}/private to exist and be 0700
+              # FIXME: I don't entirely understand why this happens sometimes... a service works, then on rebuild
+              # it tries to migrate to use a */private/* version and fails because of 755 perms. Then often requires
+              # manual 700 modification to fix if I forget to add this first.
               {
                 directory = "/var/lib/private";
                 mode = "0700";
@@ -88,11 +132,15 @@
                 directory = "/var/cache/private";
                 mode = "0700";
               }
+
             ]
             ++ lib.optional config.system.impermanence.autoPersistHomes (
               map (user: {
                 directory = "${if pkgs.stdenv.isDarwin then "/Users" else "/home"}/${user}";
                 inherit user;
+                # FIXME: Can't use config.users.users here due to recursion, despite
+                # old code using it okay?
+                #group = config.users.users.${user}.group;
                 group = if pkgs.stdenv.isDarwin then "staff" else "users";
                 mode = "u=rwx,g=,o=";
               }) config.hostSpec.users
@@ -100,9 +148,12 @@
           )
         );
         files = [
+          # Essential. If you don't have these for basic setup, you will have a bad time
           "/etc/machine-id"
           "/etc/ssh/ssh_host_ed25519_key"
           "/etc/ssh/ssh_host_ed25519_key.pub"
+
+          # Non-essential
           "/root/.ssh/known_hosts"
         ];
       };
