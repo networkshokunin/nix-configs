2a101876ff940750e7bdf0bfe738cdc4ea38523a
David Lim <david@networkshokunin.com>
2026-03-22 21:28:39 +0800

    updated

--- modules/hosts/nixos/impermanence/default.nix
+++ modules/hosts/nixos/impermanence/default.nix
@@ -52,62 +52,63 @@
         script = pkgs.writeShellScript "rollback" (builtins.readFile ./rollback.sh);
       in "${script}";
     };
+  };
   
-      fileSystems."${config.hostSpec.persistFolder}".neededForBoot = true;
+    fileSystems."${config.hostSpec.persistFolder}".neededForBoot = true;
 
-      # NOTE: This is a list of directories and files that we want to persist across reboots for all systems
-      # per-system lists are provided in hosts/<host>/
-      environment.persistence."${config.hostSpec.persistFolder}" = {
-        hideMounts = true; # Temporary disable for debugging
-        directories = (
-          lib.flatten (
-            [
-              "/var/log"
-              "/var/lib/bluetooth" # move to bluetooth-specific
-              "/var/lib/nixos"
-              "/var/lib/systemd/coredump"
-              "/etc/NetworkManager/system-connections"
+    # NOTE: This is a list of directories and files that we want to persist across reboots for all systems
+    # per-system lists are provided in hosts/<host>/
+    environment.persistence."${config.hostSpec.persistFolder}" = {
+      hideMounts = true; # Temporary disable for debugging
+      directories = (
+        lib.flatten (
+          [
+            "/var/log"
+            "/var/lib/bluetooth" # move to bluetooth-specific
+            "/var/lib/nixos"
+            "/var/lib/systemd/coredump"
+            "/etc/NetworkManager/system-connections"
 
-              # systemd DynamicUser requires /var/{lib,cache}/private to exist and be 0700
-              # FIXME: I don't entirely understand why this happens sometimes... a service works, then on rebuild
-              # it tries to migrate to use a */private/* version and fails because of 755 perms. Then often requires
-              # manual 700 modification to fix if I forget to add this first.
-              {
-                directory = "/var/lib/private";
-                mode = "0700";
-              }
-              {
-                directory = "/var/cache/private";
-                mode = "0700";
-              }
+            # systemd DynamicUser requires /var/{lib,cache}/private to exist and be 0700
+            # FIXME: I don't entirely understand why this happens sometimes... a service works, then on rebuild
+            # it tries to migrate to use a */private/* version and fails because of 755 perms. Then often requires
+            # manual 700 modification to fix if I forget to add this first.
+            {
+              directory = "/var/lib/private";
+              mode = "0700";
+            }
+            {
+              directory = "/var/cache/private";
+              mode = "0700";
+            }
 
-            ]
-            ++ lib.optional config.system.impermanence.autoPersistHomes (
-              map (user: {
-                directory = "${if pkgs.stdenv.isDarwin then "/Users" else "/home"}/${user}";
-                inherit user;
-                # FIXME: Can't use config.users.users here due to recursion, despite
-                # old code using it okay?
-                #group = config.users.users.${user}.group;
-                group = if pkgs.stdenv.isDarwin then "staff" else "users";
-                mode = "u=rwx,g=,o=";
-              }) config.hostSpec.users
-            )
+          ]
+          ++ lib.optional config.system.impermanence.autoPersistHomes (
+            map (user: {
+              directory = "${if pkgs.stdenv.isDarwin then "/Users" else "/home"}/${user}";
+              inherit user;
+              # FIXME: Can't use config.users.users here due to recursion, despite
+              # old code using it okay?
+              #group = config.users.users.${user}.group;
+              group = if pkgs.stdenv.isDarwin then "staff" else "users";
+              mode = "u=rwx,g=,o=";
+            }) config.hostSpec.users
           )
-        );
-        files = [
-          # Essential. If you don't have these for basic setup, you will have a bad time
-          "/etc/machine-id"
-          "/etc/ssh/ssh_host_ed25519_key"
-          "/etc/ssh/ssh_host_ed25519_key.pub"
+        )
+      );
+      files = [
+        # Essential. If you don't have these for basic setup, you will have a bad time
+        "/etc/machine-id"
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
 
-          # Non-essential
-          "/root/.ssh/known_hosts"
-        ];
-      };
-
-      programs.fuse.userAllowOther = true;
-
-      #environment.systemPackages = [ btrfs-diff ];
+        # Non-essential
+        "/root/.ssh/known_hosts"
+      ];
     };
+
+    programs.fuse.userAllowOther = true;
+
+    #environment.systemPackages = [ btrfs-diff ];
+    
 }
