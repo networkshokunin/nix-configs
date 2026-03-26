37855e2504d4b45ff4485213a80b9e22187b9895
David Lim <david@networkshokunin.com>
2026-03-26 23:23:07 +0800

    updated

---
+++ modules/hosts/nixos/persistence/technitium.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  isImpermanent = config.system ? "impermanence" && config.system.impermanence.enable;
+in
+{
+  config = lib.mkIf (config.services.technitium-dns-server.enable && isImpermanent) {
+    environment.persistence."${config.hostSpec.persistFolder}".directories = [
+      "/var/lib/technitium-dns-server"
+    ];
+  };
+}
