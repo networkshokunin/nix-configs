d61fe330a9278125f48673c20b20cc0d29f34410
David Lim <david@networkshokunin.com>
2026-03-26 22:46:02 +0800

    updated

---
+++ hosts/common/optional/services/technitium.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  services.technitium-dns-server = {
+    enable = true;
+    openFirewall = true;
+    # Whether to open ports in the firewall. 
+    # Standard ports are 53 (UDP and TCP, for DNS), 5380 and 53443 (TCP, HTTP and HTTPS for web interface). 
+    # Specify different or additional ports in options firewallUDPPorts and firewallTCPPorts if necessary.
+  };
+}
