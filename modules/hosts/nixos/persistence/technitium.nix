03c1d361f184818a05a6f3e646be7ec8c0e26df7
David Lim <david@networkshokunin.com>
2026-03-28 14:08:52 +0800

    updated

--- modules/hosts/nixos/persistence/technitium.nix
+++ modules/hosts/nixos/persistence/technitium.nix
@@ -7,9 +7,9 @@ let
   isImpermanent = config.system ? "impermanence" && config.system.impermanence.enable;
 in
 {
-  config = lib.mkIf (config.services.technitium-dns-server.enable && isImpermanent) {
-    environment.persistence."${config.hostSpec.persistFolder}".directories = [
-      "/var/lib/private/technitium-dns-server"
-    ];
-  };
+  # config = lib.mkIf (config.services.technitium-dns-server.enable && isImpermanent) {
+  #   environment.persistence."${config.hostSpec.persistFolder}".directories = [
+  #     "/var/lib/private/technitium-dns-server"
+  #   ];
+  # };
 }
