70dede62362257f5acd5fbef4b030debb127843c
David Lim <david@networkshokunin.com>
2026-04-01 22:21:21 +0800

    updated

--- hosts/common/optional/services/netbox.nix
+++ hosts/common/optional/services/netbox.nix
@@ -15,7 +15,13 @@ in
   services.nginx.virtualHosts."netbox.${acmeConfig.domain}" = {
     forceSSL = true;
     useACMEHost = "${acmeConfig.domain}";
-    serverName = "netbox.${acmeConfig.domain}";
+    extraConfig = ''
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+      proxy_set_header X-Forwarded-Host $host;
+    '';
     locations = {
       "/" = {
           proxyPass = "http://${config.services.netbox.listenAddress}:${toString config.services.netbox.port}";
