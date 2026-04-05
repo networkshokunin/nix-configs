3ec06b97fa479131544065ab18cdd1ac0e1583b3
David Lim <david@networkshokunin.com>
2026-04-05 23:22:05 +0800

    updated

--- hosts/common/optional/services/fingerprint.nix
+++ hosts/common/optional/services/fingerprint.nix
@@ -4,4 +4,6 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  security.pam.services.login.fprintAuth = true;
+  security.pam.services.sudo.fprintAuth = true;
 }
