e515fbedd364e58cd71835b97b91125cf9ee6524
David Lim <david@networkshokunin.com>
2026-04-06 16:47:50 +0800

    updated

--- hosts/common/optional/services/fingerprint.nix
+++ hosts/common/optional/services/fingerprint.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+let
+  isImpermanent = config.system ? "impermanence" && config.system.impermanence.enable;
+in
+{
 
   #reference: https://wiki.nixos.org/wiki/Fingerprint_scanner
   services.fprintd.enable = true;
@@ -6,4 +10,8 @@
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
   security.pam.services.login.fprintAuth = true;
   security.pam.services.sudo.fprintAuth = true;
+
+  environment.persistence."${config.hostSpec.persistFolder}".directories = lib.mkIf isImpermanent [
+      "/var/lib/fprint"
+    ];
 }
