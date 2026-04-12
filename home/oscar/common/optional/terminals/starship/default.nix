7a0b3daf111ca9c87994986c72f9b50a69d2230c
David Lim <david@networkshokunin.com>
2026-04-12 17:24:21 +0800

    updated

--- home/oscar/common/optional/terminals/starship/default.nix
+++ home/oscar/common/optional/terminals/starship/default.nix
@@ -7,7 +7,7 @@
     enableBashIntegration = true;
 
     settings = {
-      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$golang$python[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)$character";
+      format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$golang$python[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)$character";
 
       directory = {
         style = "fg:#e3e5e5 bg:#769ff0";
