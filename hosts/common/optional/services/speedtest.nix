61275056c1e8a64dee19ec509d1c832937ca5780
David Lim <david@networkshokunin.com>
2026-04-13 23:26:49 +0800

    Added speedtest

--- hosts/common/optional/services/speedtest.nix
+++ hosts/common/optional/services/speedtest.nix
@@ -21,7 +21,9 @@ in
   services.nginx.virtualHosts."speedtest.${acmeConfig.domain}" = {
     useACMEHost = "${acmeConfig.domain}";
 	  extraConfig = ''
-		  proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
 	  '';
   };
 }
