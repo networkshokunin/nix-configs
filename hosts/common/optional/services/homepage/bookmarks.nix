58b3f153fe04054f86585d1a7725a0636cf08978
David Lim <david@networkshokunin.com>
2026-04-16 21:10:15 +0800

    Added dashboard

--- hosts/common/optional/services/homepage/bookmarks.nix
+++ hosts/common/optional/services/homepage/bookmarks.nix
@@ -1,22 +1,22 @@
 { inputs, ... }:
 let
-  nix-var-acmePath = "${inputs.nix-secrets}/nix-vars/acme.nix";
-  acmeConfig = import "${nix-var-acmePath}";
+  # Keep as a path type to avoid unnecessary string conversion
+  acmeConfig = import (inputs.nix-secrets + "/nix-vars/acme.nix");
 in  
 {
-  services.homepage-dashboard.bookmarks =  [
+  services.homepage-dashboard.bookmarks = [
     {
       "Network" = [
         {
-          "unifi" = {
-            icon = "unifi.png"; # Uses the dashboard-icons name
-            href = "https://unifi.${acmeConfig.domain}";
-            description = "Unifi Controller";
-            #ping = "unifi.${acmeConfig.domain}"; # Optional: Host to ping for status (can be used with showStats to show status on the dashboard)
-          };
+          "Unifi" = [
+            {
+              icon = "unifi.png";
+              href = "https://unifi.${acmeConfig.domain}";
+              description = "Unifi Controller";
+            }
+          ];
         }
       ];
     }
   ];
 }
-
