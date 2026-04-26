6bfe871c255f9333e1fa62db15370a256ad6b894
David Lim <david@networkshokunin.com>
2026-04-26 22:21:28 +0800

    Added sidecar

--- hosts/nixos/sidecar/default.nix
+++ hosts/nixos/sidecar/default.nix
@@ -13,7 +13,7 @@
 let 
   nix-var-networkPath = "${inputs.nix-secrets}/nix-vars/network.nix";
   netConfig = (import nix-var-networkPath { inherit lib; }) { 
-      hostname = config.hostSpec."sidecar-iot"; 
+      hostname = "sidecar-iot"; 
     };
 in
 {
